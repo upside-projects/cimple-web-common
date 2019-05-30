476cedd3abf306095c2af58c2ce830c648f6b693
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-05-30 11:08:32 +0100

    feat(typography): creates stories for headings

--- src/components/Typography/Typography.js
+++ src/components/Typography/Typography.js
@@ -6,12 +6,35 @@ export const base = css`
   color: ${applyColor};
 `;
 
-export const Display = styled.h1`
+export const title = css`
+  font-weigth: 700;
+`;
+
+export const HeadingL = styled.h1`
   ${base}
+  ${title} 
 
   font-size: 2.5rem;
   font-weigth: 600;
   line-height: 3rem;
+  letter-spacing: 0.24px;
+`;
+
+export const HeadingM = styled.h2`
+  ${base}
+  ${title}
+
+  font-size: 2rem;
+  line-height: 2.5rem;
+  letter-spacing: 0.24px;
+`;
+
+export const HeadingS = styled.h3`
+  ${base}
+  ${title}
+
+  font-size: 1.5rem;
+  line-height: 2rem;
   letter-spacing: 0.24px;
 `;
 
--- src/components/Typography/typography.stories.js
+++ src/components/Typography/typography.stories.js
@@ -2,8 +2,32 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 
-import { Display, Uppercase } from "./Typography";
+import { HeadingL, HeadingM, HeadingS, Uppercase } from "./Typography";
 
 storiesOf("Typography", module)
-  .add("Label", () => <Uppercase color="lightGrey">Oh no</Uppercase>)
-  .add("Display", () => <Display color="lightGrey">Wow a child</Display>);
+  .add("Label", () => <Uppercase>Product</Uppercase>)
+  .add("Heading", () => (
+    <div style={{ flexDirection: "column" }}>
+      <div style={{ marginBottom: "24px" }}>
+        <Uppercase>Heading L (40px)</Uppercase>
+        <HeadingL color="purple">The Complex made simple.</HeadingL>
+        <HeadingL color="greyDark">The Complex made simple.</HeadingL>
+        <HeadingL color="greyMedium">The Complex made simple.</HeadingL>
+        <HeadingL color="greyLight">The Complex made simple.</HeadingL>
+      </div>
+      <div style={{ marginBottom: "24px" }}>
+        <Uppercase>Heading M (32px)</Uppercase>
+        <HeadingM color="purple">The Complex made simple.</HeadingM>
+        <HeadingM color="greyDark">The Complex made simple.</HeadingM>
+        <HeadingM color="greyMedium">The Complex made simple.</HeadingM>
+        <HeadingM color="greyLight">The Complex made simple.</HeadingM>
+      </div>
+      <div style={{ marginBottom: "24px" }}>
+        <Uppercase>Heading S (24px)</Uppercase>
+        <HeadingS color="purple">The Complex made simple.</HeadingS>
+        <HeadingS color="greyDark">The Complex made simple.</HeadingS>
+        <HeadingS color="greyMedium">The Complex made simple.</HeadingS>
+        <HeadingS color="greyLight">The Complex made simple.</HeadingS>
+      </div>
+    </div>
+  ));
