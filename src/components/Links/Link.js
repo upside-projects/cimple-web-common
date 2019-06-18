07a79b76e942584c8a2c743cc26e93be05057dbd
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-06-18 18:13:14 +0100

    fix(link): changes link to renderInternal

--- src/components/Links/Link.js
+++ src/components/Links/Link.js
@@ -1,27 +1,27 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const Link = ({ navigateWith, to, children }) => {
-  const internal = /^\/(?!\/)/.test(to)
-  const external = /^http/.test(to)
+const Link = ({ renderInternalLink, ...props }) => {
+  const internal = /^\/(?!\/)/.test(props.to)
+  const external = /^http/.test(props.to)
 
   if (internal) {
-    return navigateWith(to)
+    return renderInternalLink(props)
   }
 
   if (external) {
-    return <a href={to}>{children}</a>
+    return <a href={props.to}>{props.children}</a>
   }
 
-  return <a href={to}>{children}</a>
+  return <a href={props.to}>{props.children}</a>
 }
 
 export default Link
 
-Link.defaultProps = { navigateWith: () => {} }
+Link.defaultProps = { renderInternalLink: () => {} }
 
 Link.propTypes = {
-  children: PropTypes.node.isRequired,
   to: PropTypes.string.isRequired,
-  navigateWith: PropTypes.func
+  children: PropTypes.node.isRequired,
+  renderInternalLink: PropTypes.func
 }
