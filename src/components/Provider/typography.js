7c1357f3b61d27cbf39013d4166f889b4810cc8b
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-06-18 14:38:34 +0100

    fix(type): removes oblique types

--- src/components/Provider/typography.js
+++ src/components/Provider/typography.js
@@ -2,18 +2,12 @@ import { css } from "styled-components"
 
 import HKGroteskRegularWoff from "./fonts/HKGrotesk-Regular.woff"
 import HKGroteskRegularWoff2 from "./fonts/HKGrotesk-Regular.woff2"
-import HKGroteskItalicWoff from "./fonts/HKGrotesk-Italic.woff"
-import HKGroteskItalicWoff2 from "./fonts/HKGrotesk-Italic.woff2"
 
 import HKGroteskMediumWoff from "./fonts/HKGrotesk-Medium.woff"
 import HKGroteskMediumWoff2 from "./fonts/HKGrotesk-Medium.woff2"
-import HKGroteskMediumItalicWoff from "./fonts/HKGrotesk-MediumItalic.woff"
-import HKGroteskMediumItalicWoff2 from "./fonts/HKGrotesk-MediumItalic.woff2"
 
 import HKGroteskSemiBoldWoff from "./fonts/HKGrotesk-SemiBold.woff"
 import HKGroteskSemiBoldWoff2 from "./fonts/HKGrotesk-SemiBold.woff2"
-import HKGroteskSemiBoldItalicWoff from "./fonts/HKGrotesk-SemiBoldItalic.woff"
-import HKGroteskSemiBoldItalicWoff2 from "./fonts/HKGrotesk-SemiBoldItalic.woff2"
 
 export default css`
   /* REGULAR */
@@ -26,15 +20,6 @@ export default css`
       url(${HKGroteskRegularWoff}) format("woff");
   }
 
-  @font-face {
-    font-display: auto;
-    font-family: "HKGrotesk";
-    font-weight: 400;
-    font-style: italic;
-    src: url(${HKGroteskItalicWoff2}) format("woff2"),
-      url(${HKGroteskItalicWoff}) format("woff");
-  }
-
   /* MEDIUM */
   @font-face {
     font-display: auto;
@@ -45,15 +30,6 @@ export default css`
       url(${HKGroteskMediumWoff}) format("woff");
   }
 
-  @font-face {
-    font-display: auto;
-    font-family: "HKGrotesk";
-    font-weight: 700;
-    font-style: italic;
-    src: url(${HKGroteskMediumItalicWoff2}) format("woff2"),
-      url(${HKGroteskMediumItalicWoff}) format("woff");
-  }
-
   /* SEMIBOLD */
   @font-face {
     font-display: auto;
@@ -62,14 +38,5 @@ export default css`
     font-weight: 800;
     src: url(${HKGroteskSemiBoldWoff2}) format("woff2"),
       url(${HKGroteskSemiBoldWoff}) format("woff");
-  }
-
-  @font-face {
-    font-display: auto;
-    font-family: "HKGrotesk";
-    font-weight: 800;
-    font-style: italic;
-    src: url(${HKGroteskSemiBoldItalicWoff2}) format("woff2"),
-      url(${HKGroteskSemiBoldItalicWoff}) format("woff");
   }
 `
