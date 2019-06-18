6867f62289cd7f153d897aa34727f5c45d785e69
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-06-18 15:21:48 +0100

    fix(exports): exports more components on index.js for bundle

--- src/components/index.js
+++ src/components/index.js
@@ -10,6 +10,7 @@ export { default as Checkbox } from "./Forms/Checkbox/Checkbox"
 export { default as ErrorMessage } from "./Forms/ErrorMessage/ErrorMessage"
 export { default as Input } from "./Forms/Input/Input"
 export { default as Label } from "./Forms/Label/Label"
+export { default as Searchbar } from "./Forms/Search/Search"
 
 export { default as Radio } from "./Forms/Radio/Radio"
 
@@ -18,10 +19,20 @@ export { default as Topbar } from "./Navigation/Topbar/Topbar"
 export { default as Footer } from "./Navigation/Footer/Footer"
 
 /* Links */
+export { default as ArrowLink } from "./Links/ArrowLink/ArrowLink"
+export { default as BackLink } from "./Links/BackLink/BackLink"
 export { default as NavLink } from "./Links/NavLink/NavLink"
+export { default as ExternalLink } from "./Links/ExternalLink/ExternalLink"
+export { default as Link } from "./Links/Link"
 
 /* Logo */
 export { default as Logo } from "./Logo/Logo"
+
+/* Sort */
+export { default as Sort } from "./Sort/Sort"
+
+/* Tag */
+export { default as Tag } from "./Tag/Tag"
 
 /* Typography */
 export {
@@ -32,6 +43,9 @@ export {
   UppercaseL,
   Uppercase
 } from "./Typography/Typography"
+
+/* Icons */
+export { default as Icons } from "./Icons"
 
 /* Layout */
 export { Flex, Column } from "./Layout/Layout"
