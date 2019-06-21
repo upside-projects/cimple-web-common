65d191eb28492e42522ab8807d744ce44c5ebb18
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-06-21 17:01:43 +0100

    fix(link): adds next link support

--- src/components/Links/Link.js
+++ src/components/Links/Link.js
@@ -1,27 +1,46 @@
 import React from "react"
 import PropTypes from "prop-types"
+import NextLink from "next/link"
 
-const Link = ({ renderInternalLink, ...props }) => {
-  const internal = /^\/(?!\/)/.test(props.to)
-  const external = /^http/.test(props.to)
+const Link = ({ internalType, to, children, ...rest }) => {
+  const internal = /^\/(?!\/)/.test(to)
+  const external = /^http/.test(to)
 
   if (internal) {
-    return renderInternalLink(props)
+    console.error(internalType)
+    switch (internalType) {
+      case "next":
+        return (
+          <NextLink href={to} {...rest}>
+            {children}
+          </NextLink>
+        )
+      default:
+        throw new Error("Internal Link Type not supported")
+    }
   }
 
   if (external) {
-    return <a href={props.to}>{props.children}</a>
+    return (
+      <a target="_blank" rel="noopener noreferrer" href={to}>
+        {children}
+      </a>
+    )
   }
 
-  return <a href={props.to}>{props.children}</a>
+  return (
+    <a target="_blank" rel="noopener noreferrer" href={to}>
+      {children}
+    </a>
+  )
 }
 
 export default Link
 
-Link.defaultProps = { renderInternalLink: () => {} }
+Link.defaultProps = { internalType: "next" }
 
 Link.propTypes = {
   to: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
-  renderInternalLink: PropTypes.func
+  internalType: PropTypes.oneOf(["next"]) // 'next', 'reach', 'gatsby', ...
 }
