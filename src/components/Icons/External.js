c86f48433917e44e6e67ef932dea6db6d5535351
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-07-10 13:23:32 +0100

    feat(icon): adds currentcolor

--- src/components/Icons/External.js
+++ src/components/Icons/External.js
@@ -10,10 +10,10 @@ const External = ({ ...props }) => {
       viewBox="0 0 24 24"
     >
       <g fill="none" fillRule="evenodd">
-        <polyline stroke="#151E29" points="12 10 12 13 4 13 4 5 7 5" />
-        <path stroke="#151E29" d="M13,4 L8,9" />
+        <polyline stroke="#currentColor" points="12 10 12 13 4 13 4 5 7 5" />
+        <path stroke="#currentColor" d="M13,4 L8,9" />
         <polyline
-          stroke="#151E29"
+          stroke="#currentColor"
           strokeLinecap="square"
           points="10 4 13 4 13 7"
         />
