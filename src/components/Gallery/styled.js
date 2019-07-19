e92127e61ff40952150879b59dfb993ac187e05e
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-07-19 10:45:10 +0100

    fix(gallery): fixes gallery width

--- src/components/Gallery/styled.js
+++ src/components/Gallery/styled.js
@@ -6,6 +6,7 @@ export const GalleryHolder = styled.div`
 
   @media only screen and (max-width: 768px) {
     width: 100%;
+    max-width: none;
   }
 `
 
--- src/components/Product/BigProduct/BigProduct.stories.js
+++ src/components/Product/BigProduct/BigProduct.stories.js
@@ -4,7 +4,7 @@ import { storiesOf } from "@storybook/react"
 
 import BigProduct from "./BigProduct"
 
-storiesOf("Product", module).add("Big Thumbnail", () => (
+storiesOf("Product", module).add("Default Big", () => (
   <BigProduct
     src="https://cdn2.shopify.com/s/files/1/0752/8831/products/ito-bindery-memo-block_3f131db0-b3a8-4a65-a89c-15e4a0842bff_1400x.jpg?v=1559703503"
     name="Stainless Steel Stapler"
@@ -12,4 +12,5 @@ storiesOf("Product", module).add("Big Thumbnail", () => (
     category="category"
   />
 ))
-storiesOf("Product", module).add("Big Thumbnail - Empty", () => <BigProduct />)
+
+storiesOf("Product", module).add("Emtpy Big", () => <BigProduct />)
