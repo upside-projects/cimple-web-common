3ef57b25189105274fab00a8a47624387b712582
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-08-05 11:44:09 +0100

    feat(close): adds close icon

---
+++ src/components/Icons/Close.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Close = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+    >
+      <g fill="none" fillRule="evenodd">
+        <rect width="20" height="20" fill="#D8D8D8" opacity="0" />
+        <path
+          stroke="#151E29"
+          d="M5.83333333 0L5.83333333 11.6666667M9.09494702e-13 5.83333333L11.6666667 5.83333333"
+          transform="rotate(45 2.887 12.946)"
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default Close
