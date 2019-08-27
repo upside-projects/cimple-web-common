3bfa860289f9931f865dd4f0b46ec5c1d9b6feb2
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-08-27 11:37:51 +0100

    feat(thumbnail): adds small props

--- src/components/Thumbnail/Thumbnail.stories.js
+++ src/components/Thumbnail/Thumbnail.stories.js
@@ -1,9 +1,20 @@
-import React from "react"
+import React from "react";
 
-import { storiesOf } from "@storybook/react"
+import { storiesOf } from "@storybook/react";
 
-import Thumbnail from "./Thumbnail"
+import Thumbnail from "./Thumbnail";
 
-storiesOf("Thumbnail", module).add("Default", () => (
-  <Thumbnail src="https://images.unsplash.com/photo-1541690161184-75cbc0386cd6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80" />
-)).add("Empty", () => <Thumbnail />)
+storiesOf("Thumbnail/Default", module)
+  .add("Default", () => (
+    <Thumbnail src="https://images.unsplash.com/photo-1541690161184-75cbc0386cd6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80" />
+  ))
+  .add("Empty", () => <Thumbnail />);
+
+storiesOf("Thumbnail/Small", module)
+  .add("Default", () => (
+    <Thumbnail
+      small
+      src="https://images.unsplash.com/photo-1541690161184-75cbc0386cd6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80"
+    />
+  ))
+  .add("Empty", () => <Thumbnail small />);
--- src/components/Thumbnail/styled.js
+++ src/components/Thumbnail/styled.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-import styled from "styled-components"
+import styled, { css } from "styled-components";
 
 export const Thumbnail = styled.div`
   background-color: ${({ theme }) => theme.colors.greyscale.lighter};
@@ -12,4 +12,12 @@ export const Thumbnail = styled.div`
   background-image: ${p => `url(${p.src})`};
   background-size: cover;
   background-position: center center;
-`
+
+  ${p =>
+    p.small &&
+    css`
+      height: 2.2rem;
+      width: 2.2rem;
+      min-width: 2.2rem;
+    `}
+`;
