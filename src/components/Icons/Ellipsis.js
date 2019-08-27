7ff44632d098a97e8a070a8c115976e9442a5d77
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-08-27 16:11:43 +0100

    feat(ellipsis): adds ellipsis icon to icons object

---
+++ src/components/Icons/Ellipsis.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Ellipsis = ({ ...props }) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <g fill="none" fill-rule="evenodd">
+        <rect width="24" height="24" fill="#D8D8D8" opacity="0" />
+        <g fill="currentColor" transform="translate(11 5)">
+          <circle cx="1" cy="7" r="1" />
+          <circle cx="1" cy="1" r="1" />
+          <circle cx="1" cy="13" r="1" />
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default Ellipsis;
--- src/components/Icons/Icons.stories.js
+++ src/components/Icons/Icons.stories.js
@@ -1,29 +1,31 @@
-import React from "react"
+import React from "react";
 
-import { storiesOf } from "@storybook/react"
+import { storiesOf } from "@storybook/react";
 
-import Icons from "./index"
+import Icons from "./index";
 
-storiesOf("Primitives | Icons", module).add("All", () => (
-  <div
-    style={{
-      display: "grid",
-      gridGap: "20px",
-      gridTemplateColumns: "20px 20px 20px",
-      gridTemplateRows: "20px 20px 20px"
-    }}
-  >
-    <Icons.Cart />
-    <Icons.CaretUp />
-    <Icons.CaretDown />
-    <Icons.CaretRight />
-    <Icons.Arrow />
-    <Icons.Danger />
-    <Icons.DoubleChevron />
-    <Icons.External />
-    <Icons.Search />
-    <Icons.Hamburger />
-    <Icons.Edit />
-  </div>
-))
-.add("Cart", () => <Icons.Cart />)
+storiesOf("Primitives | Icons", module)
+  .add("All", () => (
+    <div
+      style={{
+        display: "grid",
+        gridGap: "20px",
+        gridTemplateColumns: "20px 20px 20px",
+        gridTemplateRows: "20px 20px 20px"
+      }}
+    >
+      <Icons.Cart />
+      <Icons.CaretUp />
+      <Icons.CaretDown />
+      <Icons.CaretRight />
+      <Icons.Arrow />
+      <Icons.Danger />
+      <Icons.DoubleChevron />
+      <Icons.External />
+      <Icons.Search />
+      <Icons.Hamburger />
+      <Icons.Edit />
+      <Icons.Ellipsis />
+    </div>
+  ))
+  .add("Cart", () => <Icons.Cart />);
