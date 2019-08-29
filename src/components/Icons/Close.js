061ba119d39546429420665c4645b870b458dbb3
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-08-29 10:30:01 +0100

    fix(icons): changes close icon to use currentcolor

--- src/components/Icons/Close.js
+++ src/components/Icons/Close.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 
 const Close = () => {
   return (
@@ -11,13 +11,13 @@ const Close = () => {
       <g fill="none" fillRule="evenodd">
         <rect width="20" height="20" fill="#D8D8D8" opacity="0" />
         <path
-          stroke="#151E29"
+          stroke="currentColor"
           d="M5.83333333 0L5.83333333 11.6666667M9.09494702e-13 5.83333333L11.6666667 5.83333333"
           transform="rotate(45 2.887 12.946)"
         />
       </g>
     </svg>
-  )
-}
+  );
+};
 
-export default Close
+export default Close;
