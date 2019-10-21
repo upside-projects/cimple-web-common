c1705c18911e87c6d9c975f2749245e08e8a82c3
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-10-21 13:28:27 +0100

    fix(gooddisplau): makes thumbnail smaller on desktop

--- src/components/Goods/GoodDisplay/styled.js
+++ src/components/Goods/GoodDisplay/styled.js
@@ -1,6 +1,6 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
-import StyledTag from "../../Tag/Tag"
+import StyledTag from '../../Tag/Tag';
 
 export const ProductThumb = styled.div`
   background-color: ${({ theme }) => theme.colors.greyscale.lighter};
@@ -8,7 +8,7 @@ export const ProductThumb = styled.div`
   background-size: cover;
   background-position: center center;
 
-  height: 20rem;
+  height: 14rem;
   width: 100%;
   opacity: 1;
 
@@ -19,7 +19,7 @@ export const ProductThumb = styled.div`
     height: 10rem;
     width: 100%;
   }
-`
+`;
 
 export const Holder = styled.div`
   cursor: pointer;
@@ -30,30 +30,50 @@ export const Holder = styled.div`
       opacity: 0.6;
     }
   }
-`
+`;
 
 export const Tag = styled(StyledTag)`
-  display: table;
-`
+  display: inline-block;
+  margin-bottom: 0.5rem;
+`;
 
 export const Flex = styled.div`
   display: flex;
-  flex-direction: row;
-  align-items: top;
-  justify-content: space-between;
+  flex-direction: column;
+  align-items: flex-start;
 
   @media only screen and (max-width: 780px) {
     flex-direction: column;
-
-    ${Tag} {
-      order: -1;
-      margin-bottom: 0.5rem;
-      width: fit-content;
-    }
   }
-`
+`;
 
 export const Column = styled.div`
   display: flex;
   flex-direction: column;
-`
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(14rem, 1fr));
+  grid-gap: 2rem;
+  grid-row-gap: 2.2rem;
+  justify-self: end
+  
+  margin-bottom: 3rem;
+
+  @media only screen and (max-width: 768px) {
+    grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+    grid-gap: 1rem;
+    grid-row-gap: 1rem;
+  }
+`;
+
+export const Layout = styled.div`
+  padding: 2rem 2rem 4rem 2rem;
+  max-width: 1440px;
+  margin: auto;
+
+  @media only screen and (max-width: 768px) {
+    padding: 2rem 1rem 4rem 1rem;
+  }
+`;
