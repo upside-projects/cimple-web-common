21faa37b4bc48386187fb4426ba7845de53e8d0f
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-10-22 13:37:34 +0100

    fix(radio): adds cursor with pointer

--- src/components/Forms/Radio/radio.stories.js
+++ src/components/Forms/Radio/radio.stories.js
@@ -1,24 +1,22 @@
-import React from "react"
+import React from 'react';
 
-import { storiesOf } from "@storybook/react"
-import { State, Store } from "@sambego/storybook-state"
+import { storiesOf } from '@storybook/react';
+import { State, Store } from '@sambego/storybook-state';
 
-import Radio from "./Radio"
+import Radio from './Radio';
 
 const store = new Store({
-  checked: false
-})
+  checked: false,
+});
 
-storiesOf("Primitives | Forms / Radios", module).add("Default", () => (
-  <div style={{ display: "flex", flexDirection: "row" }}>
+storiesOf('Primitives | Forms / Radios', module).add('Default', () => (
+  <div style={{ display: 'flex', flexDirection: 'row' }}>
     <State store={store}>
       {state => (
-        <Radio
-          checked={state.checked}
-          label="Buyer"
-          onChange={() => store.set({ checked: !store.state.checked })}
-        />
+        <div>
+          <Radio checked={state.checked} label="Buyer" onChange={() => store.set({ checked: !store.state.checked })} />
+        </div>
       )}
     </State>
   </div>
-))
+));
--- src/components/Forms/Radio/styled.js
+++ src/components/Forms/Radio/styled.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components"
+import styled, { css } from 'styled-components';
 
 export const Radio = styled.span`
   position: absolute;
@@ -6,6 +6,7 @@ export const Radio = styled.span`
   left: 0;
   height: 16px;
   width: 16px;
+  cursor: pointer;
 
   background-color: ${({ theme }) => theme.colors.greyscale.white};
   border: 1px solid ${({ theme }) => theme.colors.greyscale.medium};
@@ -19,7 +20,7 @@ export const Radio = styled.span`
       background-color: ${({ theme }) => theme.colors.white};
 
       &:after {
-        content: "";
+        content: '';
         position: absolute;
         height: 8px;
         width: 8px;
@@ -31,22 +32,21 @@ export const Radio = styled.span`
         left: 3px;
       }
     `}
-`
+`;
 
 export const RadioInput = styled.input`
   position: absolute;
   opacity: 0;
-  cursor: pointer;
   height: 0;
   width: 0;
-`
+`;
 
 export const Label = styled.span`
   margin-left: 1.5rem;
   display: inline-block;
   vertical-align: bottom;
   line-height: 1;
-`
+`;
 
 export const Holder = styled.div`
   display: flex;
@@ -55,4 +55,4 @@ export const Holder = styled.div`
   justify-content: center;
 
   position: relative;
-`
+`;
