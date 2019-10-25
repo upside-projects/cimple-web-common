3f56b60401de4bf078c2c12c8f7c4a66068d8370
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-10-25 16:23:45 +0100

    fix(document): adds window to document

--- src/utils/measureText.js
+++ src/utils/measureText.js
@@ -1,10 +1,12 @@
 export const measureTextWidth = (text, font) => {
   if (typeof window !== undefined) {
-    var canvas = measureTextWidth.canvas || (measureTextWidth.canvas = document.createElement('canvas'));
+    var canvas = measureTextWidth.canvas || (measureTextWidth.canvas = window.document.createElement('canvas'));
     var context = canvas.getContext('2d');
     context.font = font;
     var metrics = context.measureText(text);
 
     return metrics.width;
   }
+
+  return '24px';
 };
