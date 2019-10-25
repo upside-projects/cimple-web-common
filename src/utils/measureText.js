6714be339cbeb023d432ddcba3f9cae1bca9460c
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2019-10-25 16:00:21 +0100

    fix(counter): adds handling for ssr rendering

--- src/utils/measureText.js
+++ src/utils/measureText.js
@@ -1,8 +1,10 @@
 export const measureTextWidth = (text, font) => {
-  var canvas = measureTextWidth.canvas || (measureTextWidth.canvas = document.createElement('canvas'));
-  var context = canvas.getContext('2d');
-  context.font = font;
-  var metrics = context.measureText(text);
+  if (typeof window !== undefined) {
+    var canvas = measureTextWidth.canvas || (measureTextWidth.canvas = document.createElement('canvas'));
+    var context = canvas.getContext('2d');
+    context.font = font;
+    var metrics = context.measureText(text);
 
-  return metrics.width;
+    return metrics.width;
+  }
 };
